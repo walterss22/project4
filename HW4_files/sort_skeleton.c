119542c3be6f4559d89bdcadeb1c5143c4801158
walterss22 <56270150+walterss22@users.noreply.github.com>
2020-10-12 22:20:23 -0400

    create

---
+++ HW4_files/sort_skeleton.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+/*
+This function returns a pointer to an array with all the numbers in the file as int64_t
+Sets *size to be the number of numbers
+*/
+int64_t* Populate(char* fname, uint64_t* size){
+    //Just allocate a bunch of memory to use up some memory for now...
+    *size = 1234;
+    return (int64_t*) malloc(1234 * 8);
+}
+
+int my_sort(int64_t* input, uint64_t size){
+    //I don't know how to sort so just return 0
+    return 0;
+}
+
+/*
+Suggested function to write, to check whether the array is sorted
+Returns 0 if not sorted, returns 1 if sorted
+*/
+int is_sorted(int64_t* input, uint64_t size){
+    //Probably not sorted so...
+    return 0;
+}
+
+int main(int argc, char** argv){
+    uint64_t n; //The input size
+    int64_t* input = Populate("./numbers.txt", &n); //gets the array
+
+    my_sort(input, n);
+    
+    //check if it's sorted.
+    int sorted = is_sorted(input, n);
+    printf("Are the numbers sorted? %s \n", sorted ? "true" : "false");
+   
+    printf("Time elapsed: %lf \n", 0.0);
+    free(input);
+}
