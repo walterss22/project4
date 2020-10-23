2340399ce6e9315f8853c55848eca9930ad442ca
walterss22 <56270150+walterss22@users.noreply.github.com>
2020-10-23 01:53:31 -0400

    works linearly

--- bsort.c
+++ bsort.c
@@ -1,7 +1,15 @@
+/*
+Author: Scott Walters
+CSCI-320
+
+This program uses omp to parallelize the bubble sort
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <time.h>
 
 
 
@@ -12,13 +20,13 @@ Sets *size to be the number of numbers
 int64_t* Populate(char* fname, uint64_t* size){
     FILE* input = fopen(fname, "r"); //open file
     fscanf(input, "%lld", size); //scan for size **set at address of n**
-    printf("size: %lld\n", *size);
+    //printf("size: %lld\n", *size);
     int64_t* array = malloc((*size) * sizeof(int64_t)); //malloc for array
-    printf("size: %lld\n", *size);
+    //printf("size: %lld\n", *size);
 
     for(uint64_t ndx = 0; ndx < *size; ndx++){ //populate array
         fscanf(input, "%lld", &array[ndx]);
-        printf("%llu: %lld\n", ndx, array[ndx]);
+        //printf("%llu: %lld\n", ndx, array[ndx]);
     }
     
     fclose(input);
@@ -29,17 +37,18 @@ int my_sort(int64_t* input, uint64_t size){
     //I don't know how to sort so just return 0
     for(uint64_t i = 0; i < size; i ++){
         for(uint64_t ndx = 0; ndx < size-1; ndx++){
-            printf("Is %lld > %lld?\n", input[ndx], input[ndx+1]);
+            //printf("Is %lld > %lld?\n", input[ndx], input[ndx+1]);
             if(input[ndx] > input[ndx + 1]){
-                printf("yes!\n");
+                //printf("yes!\n");
                 int64_t temp = input[ndx];
                 input[ndx] = input[ndx + 1];
                 input[ndx + 1] = temp;
-                printf("New order: %lld, %lld\n", input[ndx], input[ndx+1]);
+                //printf("New order: %lld, %lld\n", input[ndx], input[ndx+1]);
             } 
         }
-        printf("%lld\n", i);
+        //printf("%lld\n", i);
     }
+    return 0; 
 }
 
 /*
@@ -58,17 +67,27 @@ int is_sorted(int64_t* input, uint64_t size){
 
 int main(int argc, char** argv){
     uint64_t n; //The input size
-    printf("n address: %d\n", &n);
+    //printf("n address: %d\n", &n);
     int64_t* input = Populate("./numbers.txt", &n); //gets the array
-    printf("n: %d\n", n);
+    //printf("n: %d\n", n);
 
+    struct timespec start, end;
+    double time_diff;
+
+    clock_gettime(CLOCK_MONOTONIC, &start); 
 
     my_sort(input, n);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
     
     //check if it's sorted.
     int sorted = is_sorted(input, n);
+
+    time_diff = (end.tv_sec - start.tv_sec); //Difference in seconds
+    time_diff += (end.tv_nsec - start.tv_nsec) / 1e9; 
+
     printf("Are the numbers sorted? %s \n", sorted ? "true" : "false");
    
-    printf("Time elapsed: %lf \n", 0.0);
+    printf("Time elapsed: %lf \n", time_diff);
     free(input);
 }
--- oesort.c
+++ oesort.c
@@ -1,7 +1,15 @@
+/*
+Author: Scott Walters
+CSCI-320
+
+This program uses omp to implement an odd-even bubble sort
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <time.h>
 
 
 
@@ -12,30 +20,30 @@ Sets *size to be the number of numbers
 int64_t* Populate(char* fname, uint64_t* size){
     FILE* input = fopen(fname, "r"); //open file
     fscanf(input, "%lld", size); //scan for size **set at address of n**
-    printf("size: %lld\n", *size);
+    //printf("size: %lld\n", *size);
     int64_t* array = malloc((*size) * sizeof(int64_t)); //malloc for array
-    printf("size: %lld\n", *size);
+    //printf("size: %lld\n", *size);
 
     for(uint64_t ndx = 0; ndx < *size; ndx++){ //populate array
         fscanf(input, "%lld", &array[ndx]);
-        printf("%llu: %lld\n", ndx, array[ndx]);
+        //printf("%llu: %lld\n", ndx, array[ndx]);
     }
     
-    fclose(input);
-    return array;
+    fclose(input); //close file
+    return array; //return array
 }
 
 int my_sort(int64_t* input, uint64_t size){
     //I don't know how to sort so just return 0
-    for(uint64_t i = 0; i < size; i ++){
-        for(uint64_t ndx = i % 2; ndx < size-1; ndx += 2){
-            printf("Is %lld > %lld?\n", input[ndx], input[ndx+1]);
-            if(input[ndx] > input[ndx + 1]){
-                printf("yes!\n");
+    for(uint64_t i = 0; i < size; i ++){ //outer loop
+        for(uint64_t ndx = i % 2; ndx < size-1; ndx += 2){ //inner loop
+            //printf("Is %lld > %lld?\n", input[ndx], input[ndx+1]);
+            if(input[ndx] > input[ndx + 1]){ //check for swap
+                //printf("yes!\n");
                 int64_t temp = input[ndx];
                 input[ndx] = input[ndx + 1];
                 input[ndx + 1] = temp;
-                printf("New order: %lld, %lld\n", input[ndx], input[ndx+1]);
+                //printf("New order: %lld, %lld\n", input[ndx], input[ndx+1]);
             } 
         }
         //printf("%lld\n", i);
@@ -52,7 +60,6 @@ int is_sorted(int64_t* input, uint64_t size){
         if(input[ndx] > input[ndx + 1]){
             return 0;
         }
-
     }
     return 1;
 }
@@ -61,19 +68,28 @@ int main(int argc, char** argv){
     uint64_t n; //The input size
     //printf("n address: %d\n", &n);
     int64_t* input = Populate("./numbers.txt", &n); //gets the array
-    printf("n: %llu\n", n);
+    //printf("n: %llu\n", n);
+    struct timespec start, end;
+    double time_diff;
 
-    
+    clock_gettime(CLOCK_MONOTONIC, &start); 
+
     my_sort(input, n);
 
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    /*
     for(int ndx = 0; ndx < n; ndx++){
         printf("%d: %llu\n", ndx, input[ndx]);
     }
+    */
     
     //check if it's sorted.
     int sorted = is_sorted(input, n);
     printf("Are the numbers sorted? %s \n", sorted ? "true" : "false");
+
+    time_diff = (end.tv_sec - start.tv_sec); //Difference in seconds
+    time_diff += (end.tv_nsec - start.tv_nsec) / 1e9; 
    
-    printf("Time elapsed: %lf \n", 0.0);
+    printf("Time elapsed: %lf \n", time_diff);
     free(input);
 }
