8cb42e4092dfc48320339503432f168f0e95dfb4
walterss22 <56270150+walterss22@users.noreply.github.com>
2020-10-23 14:56:56 -0400

    move to proper folders

--- Walters_project4/oesort.c
+++ Walters_project4/oesort.c
@@ -36,7 +36,7 @@ int64_t* Populate(char* fname, uint64_t* size){
 
 int my_sort(int64_t* input, uint64_t size){
     //I don't know how to sort so just return 0
-    //#pragma omp parallel for
+    #pragma omp parallel for
         for(uint64_t i = 0; i < size; i ++){ //outer loop
             for(uint64_t ndx = i % 2; ndx < size-1; ndx += 2){ //inner loop
                 //printf("Is %lld > %lld?\n", input[ndx], input[ndx+1]);
