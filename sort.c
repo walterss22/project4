be17567949bfe99f44b364537edeecc6fc535c85
walterss22 <56270150+walterss22@users.noreply.github.com>
2020-10-22 22:45:00 -0400

    bubble sort done

--- sort.c
+++ sort.c
@@ -10,18 +10,15 @@ This function returns a pointer to an array with all the numbers in the file as 
 Sets *size to be the number of numbers
 */
 int64_t* Populate(char* fname, uint64_t* size){
-    FILE* input = fopen(fname, "r");
+    FILE* input = fopen(fname, "r"); //open file
+    fscanf(input, "%lld", size); //scan for size **set at address of n**
+    printf("size: %lld\n", *size);
+    int64_t* array = malloc((*size) * sizeof(int64_t)); //malloc for array
+    printf("size: %lld\n", *size);
 
-    size = malloc(sizeof(uint64_t));
-    fscanf(input, "%d", &size);
-    printf("File name: %s\n", fname);
-    printf("size: %d\n", size);
-    int64_t* array = malloc(sizeof(int));
-    printf("malloc done\n");
-    for(uint64_t ndx = 0; ndx < *size; ndx++){
-        fscanf(input, "%d", &array[ndx]);
-        printf("scanned %d\n", ndx);
-        //printf("%llu : %lld", ndx, array[ndx]);
+    for(uint64_t ndx = 0; ndx < *size; ndx++){ //populate array
+        fscanf(input, "%lld", &array[ndx]);
+        printf("%llu: %lld\n", ndx, array[ndx]);
     }
     
     fclose(input);
@@ -30,7 +27,19 @@ int64_t* Populate(char* fname, uint64_t* size){
 
 int my_sort(int64_t* input, uint64_t size){
     //I don't know how to sort so just return 0
-    return 0;
+    for(uint64_t i = 0; i < size; i ++){
+        for(uint64_t ndx = 0; ndx < size-1; ndx++){
+            printf("Is %lld > %lld?\n", input[ndx], input[ndx+1]);
+            if(input[ndx] > input[ndx + 1]){
+                printf("yes!\n");
+                int64_t temp = input[ndx];
+                input[ndx] = input[ndx + 1];
+                input[ndx + 1] = temp;
+                printf("New order: %lld, %lld\n", input[ndx], input[ndx+1]);
+            } 
+        }
+        printf("%lld\n", i);
+    }
 }
 
 /*
@@ -38,13 +47,21 @@ Suggested function to write, to check whether the array is sorted
 Returns 0 if not sorted, returns 1 if sorted
 */
 int is_sorted(int64_t* input, uint64_t size){
-    //Probably not sorted so...
-    return 0;
+    for(uint64_t ndx = 0; ndx < size-1; ndx ++){
+        if(input[ndx] > input[ndx + 1]){
+            return 0;
+        }
+
+    }
+    return 1;
 }
 
 int main(int argc, char** argv){
     uint64_t n; //The input size
+    printf("n address: %d\n", &n);
     int64_t* input = Populate("./numbers.txt", &n); //gets the array
+    printf("n: %d\n", n);
+
 
     my_sort(input, n);
     
